093e6c6028bf675e27e9a4c599eb7e63cc4bf3ea
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-07-09 16:28:51 +0100

    03 exercise completed added

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const reverseText = str.split("").reduce((acc, char) => char + acc, "");
+    return reverseText;
 };
+
+reverseString("hello there");
 
 // Do not edit below this line
 module.exports = reverseString;
