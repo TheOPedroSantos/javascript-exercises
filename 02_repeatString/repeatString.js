ad32ab8465d774920c7b643925829ae7224e04c4
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-07-09 15:04:55 +0100

    02 exercise completed added

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let result = '';
+    for (i = 0; i < num; i++) {
+        result += str;
+    }    
+    return result;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
