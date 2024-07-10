91e9d7446cf311f0a60f798fde5dfd8d2475c611
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-07-10 16:40:25 +0100

    05 exercise added incompleted

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, secondInt) {
+    const result = 0;
+    for (let i = firstInt; i = secondInt; i++) {
+        result =+ i;
+    }
+return result;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
