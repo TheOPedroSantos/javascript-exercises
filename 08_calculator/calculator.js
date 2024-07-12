c54fa0eaa18fced93962e5cf8a408bfa38007ce4
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-07-12 16:12:48 +0100

    08 incompleted exercise added

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,43 @@
-const add = function() {
-	
+const add = function(input1, input2) {
+	result = (input1 + input2);
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(input1, input2) {
+	result = (input1 - input2);
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = 0;
+  for ( let i = 0; i < array.length; i++) {
+    result += array[i];
+  }
+  return result;
 };
 
-const multiply = function() {
+// const sum = function(array) {
+//   return array.reduce((total, current) => total + current, 0);
+// };
 
+const multiply = function(array) {
+  let result = 1;
+  for (i = 0; i < array.length; i++) {
+    result *= array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+// const multiply = function(array) {
+//   return array.reduce((total, current) => total * current, 0);
+// };
+
+const power = function(input1, input2) {
+	let result = 1;
+   
 };
 
-const factorial = function() {
+const factorial = function(input1, input2) {
 	
 };
 
