e7fa49f560b4fe0fc28518292043da74b67f028e
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-07-12 14:41:09 +0100

    07 exercise completed added

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahTemp) {
+  const result = ((fahTemp - 32) * 0.5556);
+  if (result % 1 != 0) {
+    return result.toFixed(1);
+  } else {
+    return result;
+  };
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celTemp) {
+  const result = ((celTemp * 1.8) + 32);
+  if (result % 1 != 0) {
+    return result.toFixed(1);
+  } else {
+    return result;
+  }
 };
+
+convertToCelsius(32);
+convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
