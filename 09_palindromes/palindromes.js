c39738e9572788f587ff3cdba0d5540356ba8829
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-07-15 14:47:07 +0100

    09 exercise completed added

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+    str = str.replace(/\s/g, '').toLowerCase();
+    const reverseString = str.split("").reverse().join("");
+    return str === reverseString;
 };
+
+palindromes('racecar');
 
 // Do not edit below this line
 module.exports = palindromes;
