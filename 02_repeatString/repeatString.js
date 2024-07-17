e3669ae2af86bd287f60dec6b495035cacaf8295
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-07-17 15:39:35 +0100

    02 Exercise all completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,23 @@
+// Repeats a string a number of times
+// and returns an 'ERROR' with negative numbers
+
 const repeatString = function(str, num) {
     let result = '';
-    for (i = 0; i < num; i++) {
-        result += str;
-    }    
-    return result;
+    if (num >= 1) {
+        for (i = 0; i < num; i++) {
+            result += str;
+        } return result;
+    } else if (num === 0) {
+        result = '';
+        return result;
+    } else {
+        result = 'ERROR';
+        return result;
+    }
 };
 
 repeatString("hey", 3);
+
 
 // Do not edit below this line
 module.exports = repeatString;
