8e8e93e465fdf49fc9939ca768f54f67ff27bbe3
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-07-17 16:52:36 +0100

    05 exercise all completed added

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,6 +2,12 @@ const sumAll = function(start, end) {
     if (start > end) {
         [start, end] = [end, start];
     }
+    if (isNaN(start) || isNaN(end) || typeof start !== 'number' || typeof end !== 'number') {
+        return 'ERROR'
+    }
+    if (start < 0 || end < 0) {
+        return 'ERROR'
+    }
     let sum = 0;
     for (let i = start; i <= end; i++) {
         sum += i;
@@ -9,7 +15,7 @@ const sumAll = function(start, end) {
     return sum;
 };
 
-console.log(sumAll(1, 4));
+// typeof start !== 'number' || typeof end !== 'number' || isNaN(start) || isNaN(end)
 
 // Do not edit below this line
 module.exports = sumAll;
